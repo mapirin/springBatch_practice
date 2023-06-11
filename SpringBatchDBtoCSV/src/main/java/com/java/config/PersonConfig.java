fda4c9736570d43afe159282c9a2ec444f4c3301
hayashi <812matahiro29@gmail.com>
2023-06-11 22:02:14 +0900

    ■details
    flatFileItemReader
    	→読み込み処理を実装
    	→Cドライブ下の..\springBatch_practice\take1内にあるCSVファイルを読み込む
    	→names領域をマッピングする
    	→build
    //TODO
    FlatFileReaderの実装に修正する

--- SpringBatchDBtoCSV/src/main/java/com/java/config/PersonConfig.java
+++ SpringBatchDBtoCSV/src/main/java/com/java/config/PersonConfig.java
@@ -1,6 +1,8 @@
 package com.java.config;
 
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -10,13 +12,25 @@ import com.java.form.Person;
 @Configuration
 public class PersonConfig {
 	
+	/**
+	 * csvファイルを読み込むクラス
+	 * 
+	 * @return
+	 */
 	@Bean
 	public FlatFileItemReader<Person> reader(){
-		FlatFileItemReader<Person> reader=new FlatFileItemReader<Person>();
-		reader.setResource(new FileSystemResource("\\C:\\springBatch_practice\\take1\\personDate.csv"));
-		
 		//TODO
-		//readerの具体的な実装を記述する
-		return reader;
+		//builderではなくFlatFileReaderの実装に修正する
+		//詳細はSpring batch リファレンスを参照
+		return new FlatFileItemReaderBuilder<Person>()
+				.name(null)
+				.resource(new FileSystemResource("\\C:\\springBatch_practice\\take1\\personDate.csv"))
+				.delimited()
+				.names(new String[] {"personID","firstName","lastName","gender","age","country"})
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<Person>(){
+					{
+						setTargetType(Person.class);
+					}
+				}).build();
 	}
 }
