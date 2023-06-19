d8478801e7c7330ed8211ee779ee0678c893059c
hayashi <812matahiro29@gmail.com>
2023-06-19 17:48:11 +0900

    2023/06/19
    cloneしたspring_batch_exampleに対して以下改修
    
    ・I/OをCSVからDB→CSVからCSV
    ・各メソッドの処理内容をコメント記載
    ・TODOを複数追加
    ・Javaのパッケージ名を変更
    ・リソースのパッケージ追加
    ・pom.xmlにthymeleafの依存関係追加
    ・RestController→Controllerに変更
    	Batch処理開始用の画面作成
    	コントローラ作成
    		Batch実行時にエラー
    			→Unknown return value type:
    			org.springframework.batch.core.BatchStatus

---
+++ spring-batch-example-1-master/src/main/java/com/springbatchexample1/batch/Processor.java
@@ -0,0 +1,43 @@
+package com.springbatchexample1.batch;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import com.springbatchexample1.model.User;
+
+@Component
+public class Processor implements ItemProcessor<User, User> {
+
+    private static final Map<String, String> DEPT_NAMES =
+            new HashMap<>();
+
+    /*
+     * Deptの対応Mapを設定
+     */
+    public Processor() {
+        DEPT_NAMES.put("001", "Technology");
+        DEPT_NAMES.put("002", "Operations");
+        DEPT_NAMES.put("003", "Accounts");
+    }
+
+    /**
+     * 呼び出しユーザのDeptCode取得
+     * 対応Mapと取得したコードからDept取得しセット
+     * Timeをセット
+     */
+    @Override
+    public User process(User user) throws Exception {
+        String deptCode = user.getDept();
+        String dept = DEPT_NAMES.get(deptCode);
+        user.setDept(dept);
+        user.setTime(new Date());
+        //TODO
+        //ログ出力実装
+        System.out.println(String.format("Converted from [%s] to [%s]", deptCode, dept));
+        return user;
+    }
+}
---
+++ spring-batch-example-1-master/src/main/java/com/springbatchexample1/controller/LoadController.java
@@ -0,0 +1,54 @@
+package com.springbatchexample1.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoadController {
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job job;
+
+    @RequestMapping("/start-batch")
+    public String title() {
+    	return "controller/springbatchexample1";
+    }
+    
+    @RequestMapping("/load")
+    public BatchStatus load() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+
+
+        Map<String, JobParameter> maps = new HashMap<>();
+        maps.put("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters parameters = new JobParameters(maps);
+        JobExecution jobExecution = jobLauncher.run(job, parameters);
+
+        System.out.println("JobExecution: " + jobExecution.getStatus());
+
+        System.out.println("Batch is Running...");
+        //TODO
+        //JobExecutionの複数実行
+        while (jobExecution.isRunning()) {
+            System.out.println("...");
+        }
+
+        return jobExecution.getStatus();
+    }
+}
