d48706f77fa8f07feac76223e81bf1ef0bb36216
hayashi <812matahiro29@gmail.com>
2023-06-20 22:34:16 +0900

    2023/06/20
    ・loadControllerのloadメソッドに@ResponseBodyを付与
    	→@RequestMappingと合わせることで@RestControllerと同等の実装が可能
    
    	※	RestControllerは戻り値をJSON形式で返す。
    		つまり戻り値にController同様のパスを設定すると、パス自体が返され画面に表示される。
    		このためthymeleaf等を用いた画面レンダリングは行われない。
    
    		ControllerとRestControllerを組み合わせたクラスを用いたい場合は以下
    			・クラスには@Controllerを付与
    			・@RequestMapping＋@ResponseBody->@RestControllerのため、
    			　必要なメソッドに上記2アノテーションを付与してやれば、1クラスで実装可能。

--- spring-batch-example-1-master/src/main/java/com/springbatchexample1/controller/LoadController.java
+++ spring-batch-example-1-master/src/main/java/com/springbatchexample1/controller/LoadController.java
@@ -16,6 +16,7 @@ import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoadController {
@@ -32,6 +33,7 @@ public class LoadController {
     }
     
     @RequestMapping("/load")
+    @ResponseBody
     public BatchStatus load() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
 
 
